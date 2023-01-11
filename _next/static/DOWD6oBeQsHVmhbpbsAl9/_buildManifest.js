542ed24cfd21cf208a6aa5371073e43f7b50278d
Pasquale-Favella <pasquale.favella@gmail.com>
2023-01-11 17:05:31 +0000

    Deploying to gh-pages from @ Pasquale-Favella/pasquale-favella.github.io@f8a7926af7b05c117448d33ae22aa87a7d69aecf 🚀

---
+++ _next/static/DOWD6oBeQsHVmhbpbsAl9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-b55c842550e86c72.js","static/chunks/pages/index-91ca595d3d6bba97.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
