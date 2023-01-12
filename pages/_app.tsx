511a7ffcefef563397c3c084153347006c3a0101
Favella Pasquale <pasquale.favella@nttdata.com>
2023-01-12 14:46:36 +0100

    added base next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo';
 import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo 
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://pasquale-favella.github.io/',
+          siteName: 'Pasquale blogfolio',
+        }}
+        titleTemplate = 'Pasquale blogfolio | %s'
+        defaultTitle="Pasquale blogfolio"
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href:'/favicon.ico',
+          },
+        ]}
+      />
       <Component {...pageProps} />
     </Layout>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,14 @@ import PostList from '@/components/PostList';
 import GithubService from '@/services/github.service';
 import { GithubIssue } from '@/types';
 import { GetStaticProps } from 'next';
-import Head from 'next/head'
+import { NextSeo } from 'next-seo';
 
 type Props = {posts:GithubIssue[]}
 
 const Home : React.FC<Props>  = ({posts})=>{
   return (
     <>
-      <Head>
-        <title>Pasquale blogfolio</title>
-        <meta name="description" content="Pasquale blogfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo description='Pasquale Favella Blogfolio home'/>
       <main >
         
       <h1 className="text-3xl font-bold underline ">
