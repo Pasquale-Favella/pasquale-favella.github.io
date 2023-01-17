272e8cff5765015bae33448dfb97b9f0b9579cb5
Favella Pasquale <pasquale.favella@nttdata.com>
2023-01-17 12:50:40 +0100

    some home section improve

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -2,18 +2,28 @@ import React from 'react'
 import Link from 'next/link'
 import { CONSTANTS } from '@/config'
 import DateUtils from '@/utils/DateUtils'
+import Typewriter from 'typewriter-effect'
+import { BsArrowRight } from 'react-icons/bs';
 
 
 
 const HeroSection = () => {
 
     return (
-        <section className='space-y-6 md:my-16'>
+        <section className='space-y-6 mb-10 md:my-16'>
           <div className='flex flex-col-reverse gap-8 md:flex-row md:justify-between'>
             <div className='space-y-4 md:max-w-lg'>
 
-              <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                Hi, I am <span className="text-primary">Pasquale</span>
+              <h1 className="pb-6 text-3xl font-extrabold leading-9 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 flex md:inline-block gap-2">
+                Hi, I am <span className="text-primary">
+                  <Typewriter
+                    options={{
+                      strings: ['Pasquale' , 'Musician', 'Engineer'],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </span>
                 </h1>
               <h2 className='text-lg font-medium text-primary'>
                 {DateUtils.calculateAge()} yrs • Musician • Full-stack Developer
@@ -23,6 +33,9 @@ const HeroSection = () => {
               I am a highly passionated for creating web and mobile applications.           
               Let&apos;s work together to bring your ideas to life and create an exceptional product.
               </p>
+              <a className="link link-primary text-xl group" aria-label="Email to pasquale.favella@gmail.com" title="Email to pasquale.favella@gmail.com" href="mailto:pasquale.favella@gmail.com">
+                <span className='flex items-center gap-x-1'>Get in touch <BsArrowRight className='h-4 w-4 transition duration-200 group-hover:translate-x-1' /></span>
+              </a>
             </div>
             <div >
                 <div className="mockup-code">
