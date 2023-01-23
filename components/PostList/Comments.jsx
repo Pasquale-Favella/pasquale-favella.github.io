52f35dd68f90db7280d656b0fa2b30eba00aedbf
Favella Pasquale <pasquale.favella@nttdata.com>
2023-01-23 17:48:51 +0100

    giscus theme adjust

--- components/PostList/Comments.jsx
+++ components/PostList/Comments.jsx
@@ -1,33 +1,13 @@
 import { GISCUS_CONFIG } from '@/config/giscus'
-import { useRef , useEffect , useState} from 'react'
 import Giscus from '@giscus/react'
 
 
 
 const Comments = () => {
 
-  const mountRef = useRef(false);
-
-  const [giscusTheme , setGiscusTheme] = useState('dark');
-
-  useEffect(() => {
-
-    if(mountRef.current) return;
-
-   const current =  document.documentElement.getAttribute('data-theme');
-
-   if(current) setGiscusTheme(current)
-
-    mountRef.current = true;
-  }, []);
-
-  
-  if (!mountRef.current) return null
-
-
   return (
     <div className='my-8'>
-      <Giscus {...GISCUS_CONFIG} theme={giscusTheme} />
+      <Giscus {...GISCUS_CONFIG} theme={'transparent_dark'} />
     </div>
   )
 }
