48325434e1fffeb1895ae2e7ed3235b141e12222
Favella Pasquale <pasquale.favella@nttdata.com>
2023-01-23 12:53:29 +0100

    spell footer change

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = ()=>{
             </a>
 
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                ©{year} • made with love 💚
+                ©{year} • made with 💚
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-4">        
                 {CONSTANTS.HERO_LINKS.map((link, i) => (
