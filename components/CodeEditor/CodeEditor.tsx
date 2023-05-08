77e2d619b3cb412d581682247edf35130cb2127d
Favella Pasquale <pasquale.favella@nttdata.com>
2023-05-08 10:35:40 +0200

    format

--- components/CodeEditor/CodeEditor.tsx
+++ components/CodeEditor/CodeEditor.tsx
@@ -4,41 +4,39 @@ import useEditor from '@/hooks/use-editor';
 import useIsMobile from '@/hooks/use-isMobile';
 import EditorTabs from './EditorTabs';
 import PreviewFrame from './PreviewFrame';
-    
 
-  
-  const CodeEditor = ()=> {
-  
-    const { isDarkMode } = useTheme();
-    const isMobile = useIsMobile();
-    const { code , setCode , preview } = useEditor()
-    
-    const handleChange = (codeValue : string|undefined)=> {
-      setCode(codeValue ?? '');
-    }
-  
-    return (
-      <>
-        <EditorTabs />
+const CodeEditor = () => {
 
-        { preview 
-          ? <PreviewFrame />
-          : <Editor
-            className='h-[calc(85vh-70px)]'
-            theme={isDarkMode ? 'vs-dark' : 'light'}
-            path={code.name}
-            language={code.language}
-            value={code.value}
-            onChange={codeValue => handleChange(codeValue)}
-            options={
-              {
-                minimap : {enabled : !isMobile} 
-              }
-            }
-          />
-        }
-      </>
-    );
+  const { isDarkMode } = useTheme();
+  const isMobile = useIsMobile();
+  const { code, setCode, preview } = useEditor()
+
+  const handleChange = (codeValue: string | undefined) => {
+    setCode(codeValue ?? '');
   }
-  
-  export default CodeEditor
+
+  return (
+    <>
+      <EditorTabs />
+
+      {preview
+        ? <PreviewFrame />
+        : <Editor
+          className='h-[calc(85vh-70px)]'
+          theme={isDarkMode ? 'vs-dark' : 'light'}
+          path={code.name}
+          language={code.language}
+          value={code.value}
+          onChange={codeValue => handleChange(codeValue)}
+          options={
+            {
+              minimap: { enabled: !isMobile }
+            }
+          }
+        />
+      }
+    </>
+  );
+}
+
+export default CodeEditor
