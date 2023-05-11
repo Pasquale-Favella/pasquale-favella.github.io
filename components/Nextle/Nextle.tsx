1754ebb3cce2e14011fdbbfddfcdefb95b79e41c
Favella Pasquale <pasquale.favella@nttdata.com>
2023-05-11 13:46:55 +0200

    resize

--- components/Nextle/Nextle.tsx
+++ components/Nextle/Nextle.tsx
@@ -9,7 +9,7 @@ const Nextle = () => {
 
   return (
     <section className="h-[87vh] md:h-[80vh] flex flex-col items-center justify-between w-full mx-auto">
-      <div className="flex justify-center items-center w-full  mb-auto grow">
+      <div className="flex justify-center items-center w-full my-auto">
         <div className="grid grid-rows-6 gap-1 md:gap-2 box-border p-2">
           {guesses.map((_, i) => (
             <Guess
