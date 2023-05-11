76567529cd01ffe0f44ea682a99b28061f0234e3
Favella Pasquale <pasquale.favella@nttdata.com>
2023-05-11 17:43:16 +0200

    bump deps

--- components/Nextle/Nextle.tsx
+++ components/Nextle/Nextle.tsx
@@ -11,12 +11,7 @@ const Nextle = () => {
     <section className="min-h-[calc(92vh-5rem)] flex flex-col items-center justify-between w-full mx-auto">
       <div className="flex justify-center items-center w-full my-auto">
         <div className="grid grid-rows-6 gap-1 md:gap-2 box-border p-2">
-          {guesses.map((_, i) => (
-            <Guess
-              key={i}
-              index={i}
-            />
-          ))}
+          {guesses.map((_, i) => <Guess key={i} index={i} />)}
         </div>
       </div>
       <Qwerty  />
