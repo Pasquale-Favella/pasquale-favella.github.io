64d26fa2d3eeb9fc4be9d4568c4904071d944bbb
Favella Pasquale <pasquale.favella@nttdata.com>
2023-05-12 09:19:48 +0200

    removed unused deps

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
-    generateRobotsTxt: true, 
-    sitemapSize: 7000
-}
