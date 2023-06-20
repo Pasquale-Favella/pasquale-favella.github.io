d5f4b71dfb9036c600d8aca3205eff447fabc9a3
Favella Pasquale <pasquale.favella@nttdata.com>
2023-06-20 16:09:17 +0200

    rich editor visual adjustments

--- utils/GenericUtils.ts
+++ utils/GenericUtils.ts
@@ -1,12 +1,12 @@
 
 const uid = ()=> crypto.getRandomValues(new Uint32Array(1)).toString()
 
-const returnBase64FromFile = async(file: File) => {
+const returnBase64FromFile = async(file: File) : Promise<string> => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function(event) {
-            return  resolve(event.target?.result);
+            return resolve(event.target?.result as string);
         };
         reader.onerror = error => reject(error);
     })
