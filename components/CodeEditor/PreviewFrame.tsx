e663f7f74ca607d9a55937931d402d924caded94
Favella Pasquale <pasquale.favella@nttdata.com>
2023-07-25 09:58:05 +0200

    code editor better preview

--- components/CodeEditor/PreviewFrame.tsx
+++ components/CodeEditor/PreviewFrame.tsx
@@ -2,21 +2,26 @@ import { memo, FC } from 'react';
 import useEditor from '@/hooks/use-editor';
 
 type PreviewFrameProps = {}
- 
-const PreviewFrame : FC<PreviewFrameProps> = ()=> {
+
+const PreviewFrame: FC<PreviewFrameProps> = () => {
     const { srcDoc } = useEditor();
 
     return (
-        <div className="h-[calc(85vh-70px)]">
-            <iframe
-                srcDoc={srcDoc}
-                title="output"
-                sandbox="allow-scripts"
-                frameBorder="0"
-                width="100%"
-                height="100%"
-            />
-      </div>
+        <div className="mockup-browser border border-base-300">
+            <div className="mockup-browser-toolbar">
+                <div className="input border border-base-300">{window.location.origin}</div>
+            </div>
+            <div className="h-[calc(85vh-100px)]">
+                <iframe
+                    srcDoc={srcDoc}
+                    title="output"
+                    sandbox="allow-scripts"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </div>
     );
 }
 
