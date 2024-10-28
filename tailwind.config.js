08ee537a077e2dc770eb41a9def72990666f1fa7
pasquale favella <pasqualefavellaaccenture@gmail.com>
2024-10-28 17:03:15 +0000

    added shadcn components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const daisyuiColorObj = require('daisyui/src/theming/index')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -17,17 +19,71 @@ module.exports = {
             }
           }
         }
-      }
+      },
+	  colors: {
+        border: daisyuiColorObj['base-content'],
+        input: daisyuiColorObj['base-content'],
+        ring: daisyuiColorObj['base-content'],
+        background: daisyuiColorObj['base-100'],
+        foreground: daisyuiColorObj['base-content'],
+        primary: {
+          DEFAULT: daisyuiColorObj['primary'],
+          foreground: daisyuiColorObj['primary-content'],
+        },
+        secondary: {
+          DEFAULT: daisyuiColorObj['secondary'],
+          foreground: daisyuiColorObj['secondary-content'],
+        },
+        destructive: {
+          DEFAULT: daisyuiColorObj['error'],
+          foreground: daisyuiColorObj['error-content'],
+        },
+        muted: {
+          DEFAULT: daisyuiColorObj['base-300'],
+          foreground: daisyuiColorObj['base-content'],
+        },
+        accent: {
+          DEFAULT: daisyuiColorObj['accent'],
+          foreground: daisyuiColorObj['accent-content'],
+        },
+        popover: {
+          DEFAULT: daisyuiColorObj['base-100'],
+          foreground: daisyuiColorObj['base-content'],
+        },
+        card: {
+          DEFAULT: daisyuiColorObj['base-100'],
+          foreground: daisyuiColorObj['base-content'],
+        },
+      },
+      borderRadius: {
+        lg: 'var(--rounded-btn)',
+        md: 'calc(var(--rounded-btn) - 2px)',
+        sm: 'calc(var(--rounded-btn) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require("daisyui"),require('@tailwindcss/typography'),require('@tailwindcss/aspect-ratio')],
+  plugins: [require("daisyui"),require('@tailwindcss/typography'),require('@tailwindcss/aspect-ratio'), require("tailwindcss-animate")],
   daisyui: {
     themes: [
       {
         dark: {
           ...require("daisyui/src/theming/themes")["dark"],
           "primary":'#5abf4e',
-          "secondary": "#1FD65F",
+          "secondary": "#00e353",
           "accent": "#D99330",
           "neutral": "#110E0E",
           "base-100": "#171212",
