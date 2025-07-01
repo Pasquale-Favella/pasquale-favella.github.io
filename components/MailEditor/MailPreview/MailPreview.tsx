03a21dfcee26b474f413463bde423f8921a5db43
pasquale favella <pasquale.favella@gmail.com>
2025-07-01 15:58:42 +0200

    fix: Adjust padding and positioning in MailPreview component for improved layout

--- components/MailEditor/MailPreview/MailPreview.tsx
+++ components/MailEditor/MailPreview/MailPreview.tsx
@@ -17,14 +17,14 @@ const MailPreview: FC<MailPreviewProps> = ({ content, screenSize }) => {
 
   return (
     <div className={`flex justify-center ${screenSize === 'desktop' ? 'w-full' : screenSize === 'tablet' ? 'w-3/4' : 'w-1/2'} mx-auto`}>
-      <div className="relative border p-4 rounded-lg min-h-48 w-full">
+      <div className="relative border rounded-lg min-h-48 w-full">
         <iframe
           title="Mail Preview"
           srcDoc={content}
           className="w-full h-full border-none"
           style={{ minHeight: '400px' }}
         />
-        <div className="absolute top-2 right-2 flex gap-1">
+        <div className="absolute top-2 right-4 flex gap-1">
           <div
             className="tooltip"
             data-tip="Fullscreen Preview"
